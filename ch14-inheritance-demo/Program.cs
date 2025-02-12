5eb1921888211079f542700e76153a6b92a04e0b
nson95 <nicksonnenberg95@gmail.com>
2025-02-12 13:44:49 -0500

    init commit

---
+++ ch14-inheritance-demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ch14_inheritance_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
