81c39ac8190d3cb79ef350fec8ad5af9fe625d5a
nson95 <nicksonnenberg95@gmail.com>
2025-02-19 16:17:09 -0500

    updated dictionary to remove bad words

--- Hangman-App/WordDictionary.cs
+++ Hangman-App/WordDictionary.cs
@@ -18,7 +18,7 @@ namespace Hangman_App
 
                            "apple", "ball", "cat", "dog", "elephant", "fish", "grape", "hat", "ice", "jump",
                     "kite", "lemon", "moon", "nose", "orange", "pen", "quick", "rain", "sun", "tree",
-                    "umbrella", "violet", "whale", "x-ray", "yellow", "zebra", "air", "blue", "cloud",
+                    "umbrella", "violet", "dolphin", "table", "yellow", "zebra", "air", "blue", "cloud",
                     "duck", "egg", "farm", "goat", "hill", "ink", "jelly", "kite", "leaf", "mouse",
                     "night", "open", "play", "quiet", "rocket", "star", "toy", "unit", "van", "wind",
                     "xylophone", "yarn", "zoo", "ant", "balloon", "candle", "doll", "elephant", "fishbowl",
@@ -26,7 +26,7 @@ namespace Hangman_App
                     "pencil", "question", "rose", "snow", "treehouse", "up", "vampire", "water", "xmas",
                     "yellowish", "zooey", "act", "bat", "clown", "door", "even", "fly", "giraffe", "hop",
                     "inch", "joy", "kangaroo", "lemonade", "mask", "noodles", "ocean", "pizza", "quilt",
-                    "rainbow", "sock", "tent", "under", "very", "watch", "x-ray", "yarn", "zipper",
+                    "rainbow", "sock", "tent", "under", "very", "watch", "whale", "yarn", "zipper",
                     "applepie", "ballerina", "catfish", "dove", "elephant", "flower", "garden", "hat",
                     "island", "jumping", "keyhole", "laugh", "mango", "necklace", "octopus", "plum",
                     "quack", "road", "snowflake", "turtle", "underwater", "vacuum", "wet", "xenon",
