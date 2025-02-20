bc7be547ad198eb64470c52c99367d061fa20f05
nson95 <nicksonnenberg95@gmail.com>
2025-02-20 09:10:08 -0500

    init commit

---
+++ WheelOfFortune/Program.cs
@@ -0,0 +1,11 @@
+﻿using Library4Nick;
+namespace WheelOfFortune
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
